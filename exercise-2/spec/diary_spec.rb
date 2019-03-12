f13c86be5c5d75c689ca62c7a22abe7b3b4f6ecd
Paul Martin <communty.hivemind@gmail.com>
2019-03-12 12:57:18 +0000

    refactor exercise 2

--- exercise-2/spec/diary_spec.rb
+++ exercise-2/spec/diary_spec.rb
@@ -6,8 +6,6 @@ describe Diary do
       title = "title"
       body = "body"
       entry = double(:entry)
-      allow(entry).to receive(:title).and_return(title)
-      allow(entry).to receive(:body).and_return(body)
       entry_class = double(:Entry)
       allow(entry_class).to receive(:new).with(title, body).and_return(entry)
       diary = Diary.new(entry_class)
@@ -22,14 +20,16 @@ describe Diary do
         { title: "Title 02", body: "Body 02" },
         { title: "Title 03", body: "Body 03" },
       ]
+      
       entry_class = double(:Entry)
+      
       entries_data.each do |e|
         entry = double(:entry)
         allow(entry).to receive(:title).and_return(e[:title])
-        allow(entry).to receive(:body).and_return(e[:body])
         allow(entry_class).to receive(:new).with(e[:title], e[:body])
           .and_return(entry)
       end
+
       diary = Diary.new(entry_class)
       entries_data.each { |e| diary.add(e[:title], e[:body]) }
       expect(diary.index).to eq entries_data.map { |e| e[:title] }.join("\n")
