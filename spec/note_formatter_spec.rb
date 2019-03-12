ccb2e8493f4b1eb0d0aba09c41af8eaf84e07ac5
Paul Martin <communty.hivemind@gmail.com>
2019-03-12 10:35:15 +0000

    update spec and .rspec

--- spec/note_formatter_spec.rb
+++ spec/note_formatter_spec.rb
@@ -4,8 +4,8 @@ describe NoteFormatter do
   describe '#format' do
     it 'returns a formatted note' do
       note = double("Note")
-      note_title = 'This is the title'
-      note_body = 'This is the body'
+      note_title = 'A Tale of Two Cities'
+      note_body = 'It was the best of times, it was the worst of times.'
       allow(note).to receive(:title).and_return(note_title)
       allow(note).to receive(:body).and_return(note_body)
 
